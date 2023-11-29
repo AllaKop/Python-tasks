8029ba5f4f455eec0055ee52cd8ebe2e6c8f6e72
Alla Kopiichenko <allchenok020894@gmail.com>
2023-11-29 15:30:51 +0200

    Task 4 done

--- Task_4_Lists.py
+++ Task_4_Lists.py
@@ -3,4 +3,22 @@
 # Use a list to stor tasks.
 
 to_do_list = []
-your_choice = 
+
+while True:
+    your_choice = int(input("Enter a number wich corresponds your choice: 1: Add Task 2: View List 3: Remove Task 4: Exit -  "))
+
+    if your_choice == 1:
+        task = str(input("Write a name of the task to add "))
+        to_do_list.append(task)
+    elif your_choice == 2:
+        print("To-do List: ", to_do_list)
+    elif your_choice == 3:
+        task = str(input("Write a name of the task to remove "))
+        if task in to_do_list:
+            to_do_list.remove(task)
+        else: 
+            raise Exception("Task {task} is not in the list.")
+    elif your_choice == 4:
+        break
+    else:
+        raise Exception("Wrong option. Please enter a number wich corresponds your choice: 1: Add Task 2: View List 3: Remove Task 4: Exit ")
---
+++ Task_5_Dictionaries.py
@@ -0,0 +1,4 @@
+# Program that simulates a basic inventory system.
+# Use a dictionary to store items and their corresponding quantities.
+# Implement options to add items, view the inventory and update quantities.
+
