ece6dc8cfd8e1cc88b9ae060219bfdf047bb2622
Alla Kopiichenko <allchenok020894@gmail.com>
2023-11-30 15:13:36 +0200

    Made a function to calculate

--- Task_1_Variables_and_Operators.py
+++ Task_1_Variables_and_Operators.py
@@ -1,10 +1,12 @@
 # Caclulates an area of a rectangle
 
+def square_rectangle(length, width):
+    if length > 0 and width > 0:
+        area_rectangle = width * length 
+        print("The area of the rectangle is:", area_rectangle)
+    else: 
+        raise Exception("Please enter positive numbers for both length and width.")
+    
 length = float(input("What is the length of the rectangle? "))
-width = float(input("What is the width of the rectangle? "))
-
-if length > 0 and width > 0:
-    area_rectangle = width * length 
-    print("The area of the rectangle is:", area_rectangle)
-else: 
-    raise Exception("Please enter positive numbers for both length and width.")
+width = float(input("What is the width of the rectangle? "))  
+square_rectangle(length, width)
