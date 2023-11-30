10a51fea86abb87b4edaa9790876b563eb6fb304
Alla Kopiichenko <allchenok020894@gmail.com>
2023-11-30 15:30:44 +0200

    Transfeormed into class

--- Task_1_Variables_and_Operators.py
+++ Task_1_Variables_and_Operators.py
@@ -1,12 +1,16 @@
 # Caclulates an area of a rectangle
 
-def square_rectangle(length, width):
-    if length > 0 and width > 0:
-        area_rectangle = width * length 
-        print("The area of the rectangle is:", area_rectangle)
-    else: 
-        raise Exception("Please enter positive numbers for both length and width.")
-    
-length = float(input("What is the length of the rectangle? "))
-width = float(input("What is the width of the rectangle? "))  
-square_rectangle(length, width)
+class Rectangle: # class - bluepring/model of the object
+
+    def __init__(self, length, width): #  defining properties - variables but in OOP
+        self.length = length
+        self.width = width
+
+    def square_rectangle(length, width): # defining method with parameters - functions but in OOP
+        if length > 0 and width > 0:
+            area_rectangle = width * length 
+            print("The area of the rectangle is:", area_rectangle)
+        else: 
+            raise Exception("Please enter positive numbers for both length and width.")
+        
+Rectangle.square_rectangle(5,8) # calling methond of the class with arguments / calling method on instance (sample of object)
