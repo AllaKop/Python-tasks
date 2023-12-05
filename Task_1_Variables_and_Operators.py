c56d603e5ca24fc7ae66581c31867c77e3739abb
Alla Kopiichenko <allchenok020894@gmail.com>
2023-12-05 20:22:19 +0200

    Changed applying inheritance and polymorphism

--- Task_1_Variables_and_Operators.py
+++ Task_1_Variables_and_Operators.py
@@ -1,16 +1,58 @@
-# Caclulates an area of a rectangle
 
-class Rectangle: # class - bluepring/model of the object
+import numpy as np
 
-    def __init__(self, length, width): #  defining properties - variables but in OOP
+class Shape():
+    
+    def calculate_area(self):
+        pass
+
+    def calculate_perimeter(self):
+        pass
+
+class Rectangle(Shape): 
+
+    def __init__(self, length, width): 
         self.length = length
         self.width = width
 
-    def square_rectangle(length, width): # defining method with parameters - functions but in OOP
-        if length > 0 and width > 0:
-            area_rectangle = width * length 
+    def calculate_area(self):
+        if self.length > 0 and self.width > 0:
+            area_rectangle = self.width * self.length 
             print("The area of the rectangle is:", area_rectangle)
         else: 
             raise Exception("Please enter positive numbers for both length and width.")
+
+    def calculate_perimeter(self):
+        if self.length > 0 and self.width > 0:
+            perimeter_rectangle = 2 * (self.width + self.length)
+            print("The perimeter of the rectangle is:", perimeter_rectangle)
+        else: 
+            raise Exception("Please enter positive numbers for both length and width.") 
+
+class Circle(Shape):
+
+    def __init__(self, radius): 
+        self.radius = radius
+
+    def calculate_area(self):
+        if self.radius > 0:
+            area_circle = np.pi * (self.radius * self.radius)
+            print("The area of the circle is:", area_circle)
+        else: 
+            raise Exception("Please enter positive number for radius")
+
+    def calculate_perimeter(self):
+        if self.radius > 0:
+            perimeter_circle = 2 * np.pi * self.radius
+            print("The perimeter of the circle is:", perimeter_circle)
+        else: 
+            raise Exception("Please enter positive number for radius") 
         
-Rectangle.square_rectangle(5,8) # calling methond of the class with arguments / calling method on instance (sample of object)
+rectangle = Rectangle(5, 8)
+circle = Circle(6)
+
+figures = [rectangle, circle]
+
+for f in figures:
+    f.calculate_area()
+    f.calculate_perimeter()
